504813456cdeca2daab922ba8625d97a6e97b4f2
Seba <pudelko.sebastian.13@gmail.com>
2022-02-11 12:53:06 +0100

    Poland business news v2 refactor properties

--- zadanie/src/main/java/sebapd/zadanie/Service.java
+++ zadanie/src/main/java/sebapd/zadanie/Service.java
@@ -1,7 +1,6 @@
 package sebapd.zadanie;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,38 +10,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 @RestController
 public class Service {
 
-    String path = "C:\\Poland business news\\news.txt";
-    RestTemplate restTemplate;
+    @Value("${apiKey}")
+    private String apiKey;
+    @Value("${apiUrl}")
+    private  String apiUrl;
+    @Value("${filePath}")
+    private  String filePath;
+    private final RestTemplate restTemplate;
+    private final Parser parser;
 
-    public Service(RestTemplate restTemplate) {
+    public Service(RestTemplate restTemplate, Parser parser) {
         this.restTemplate = restTemplate;
+        this.parser = parser;
     }
 
     @GetMapping("/")
     public String getPolandNews() throws IOException {
-        String data = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=0f5b4a48c0d3471d80c79526538dba01", String.class);
-        JSONObject obj = new JSONObject(data);
-        String  parsedArticles = parse(obj);
-        Files.writeString(Paths.get(path), parsedArticles); // If append to end of file select append option
+        String data = restTemplate.getForObject(apiUrl + apiKey, String.class);
+        String parsedArticles = parser.parse(data);
+        Files.writeString(Paths.get(filePath), parsedArticles); // If append to end of file select append option
         return parsedArticles;
-    }
-
-    private String parse(JSONObject object) {
-        StringBuilder parsedArticles = new StringBuilder();
-        JSONArray articles = object.getJSONArray("articles");
-        for (int i = 0; i < articles.length(); i++) {
-
-            String author = articles.getJSONObject(i).optString("author", "No author");
-            String title = articles.getJSONObject(i).optString("title", "No title");
-            String description = articles.getJSONObject(i).optString("description", "No description");
-
-            String parsedSingleArticle = title + ":" + description + ":" + author;
-            parsedArticles.append(parsedSingleArticle).append(System.getProperty("line.separator"));
-        }
-        return parsedArticles.toString();
     }
 }
